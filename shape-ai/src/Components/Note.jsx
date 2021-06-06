f437513dcb542c8d36747dc123d51338bfcc313a
Nuwan Wijeweera <83973154+Nuwan-Wi@users.noreply.github.com>
2021-06-06 19:09:54 +0530

    Add files via upload

---
+++ shape-ai/src/Components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Node(){
+    return (
+        <div className="note">
+           <h1>Javascript and React.js</h1>
+           <p>This was an amazing bootcamp taken by Shaurya Singha we covered everything from Scratch including javascript and React.js, HTML.</p>
+        </div>
+    );
+}
+
+export default Node;
